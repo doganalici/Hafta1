004955d1fcfe1138b0e025f4a5af274d33e78e8b
Doğan Alıcı <doganalici2144@gmail.com>
2023-07-23 13:55:51 +0300

    Add project files.

---
+++ Hafta1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hafta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi; //declaration : değişken tanımlama
+            sayi = 12; //assign : değer atama
+            int sayi2 = 1234;
+            Console.WriteLine(sayi);
+
+            {
+                //kapsam1
+                int a = 19;
+                Console.WriteLine(a);
+                {
+                    //kapsam2
+                    //short a = 34;
+                    Console.WriteLine(a);
+                }
+
+            }
+            {
+                //kapsam3
+                int a = 345;
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+
